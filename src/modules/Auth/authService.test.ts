079f710c73344d23691c39290a1f2d220d0870d1
Чернецова Татьяна <chernetsova.tatya@gmail.com>
2020-08-23 19:16:08 +0300

    add tests to authService

--- src/modules/Auth/authService.test.ts
+++ src/modules/Auth/authService.test.ts
@@ -1,38 +1,54 @@
 import { User } from "@/modules/Game";
 import { localStorageAuth } from "./authService";
 
+describe("When localStorage is empty", () => {
+	beforeEach(() => {
+		localStorage.clear();
+	});
+
+	it("should not set user to localStorage if name is not set", () => {
+		localStorageAuth.login({ name: "", gender: "male" });
+
+		expect(localStorage.getItem(localStorageAuth.userNameKey)).toBeNull();
+		expect(localStorage.getItem(localStorageAuth.userGenderKey)).toBeNull();
+	});
+
+	it("should set user nmae and gender to localStorage if user is correct", () => {
+		localStorageAuth.login({ name: "name", gender: "female" });
+
+		expect(localStorage.getItem(localStorageAuth.userNameKey)).toBe("name");
+		expect(localStorage.getItem(localStorageAuth.userGenderKey)).toBe("female");
+	});
+});
+
 describe("When user is logged in", () => {
-  beforeEach(() => {
-    const user: User = { name: "qwe", gender: "female" };
-    localStorageAuth.login(user);
-  });
+	beforeEach(() => {
+		localStorage.clear();
+		const user: User = { name: "qwe", gender: "female" };
+		localStorageAuth.login(user);
+	});
 
-  it("should set user settings to localStorage", () => {
-    expect(localStorage.getItem(localStorageAuth.userNameKey)).toBe("qwe");
-    expect(localStorage.getItem(localStorageAuth.userGenderKey)).toBe("female");
-  });
+	it("should return this user as logged in", () => {
+		const loggedUser = localStorageAuth.getLoggedInUser();
 
-  it("should return this user as logged in", () => {
-    const loggedUser = localStorageAuth.getLoggedInUser();
+		expect(loggedUser?.name).toBe("qwe");
+		expect(loggedUser?.gender).toBe("female");
+	});
 
-    expect(loggedUser?.name).toBe("qwe");
-    expect(loggedUser?.gender).toBe("female");
-  });
+	describe("and then logout", () => {
+		it("should clear user settings in localStorage", () => {
+			localStorageAuth.logout();
 
-  describe("and then logout", () => {
-    it("should clear user settings in localStorage", () => {
-      localStorageAuth.logout();
+			expect(localStorage.getItem(localStorageAuth.userNameKey)).toBeNull();
+			expect(localStorage.getItem(localStorageAuth.userGenderKey)).toBeNull();
+		});
 
-      expect(localStorage.getItem(localStorageAuth.userNameKey)).toBeNull();
-      expect(localStorage.getItem(localStorageAuth.userGenderKey)).toBeNull();
-    });
+		it("should return undefined as logged in user", () => {
+			localStorageAuth.logout();
 
-    it("should return undefined as logged in user", () => {
-      localStorageAuth.logout();
+			const loggedUser = localStorageAuth.getLoggedInUser();
 
-      const loggedUser = localStorageAuth.getLoggedInUser();
-
-      expect(loggedUser).toBeUndefined();
-    });
-  });
+			expect(loggedUser).toBeUndefined();
+		});
+	});
 });
--- src/modules/Auth/authService.ts
+++ src/modules/Auth/authService.ts
@@ -1,36 +1,40 @@
 import { User, Gender } from "@/modules/Game";
 
 interface AuthService {
-  login: (user: User) => void;
-  getLoggedInUser: () => User | undefined;
-  logout: () => void;
+	login: (user: User) => void;
+	getLoggedInUser: () => User | undefined;
+	logout: () => void;
 }
 
 class LocalStorageAuthService implements AuthService {
-  readonly userNameKey: string = "userName";
-  readonly userGenderKey: string = "userGender";
+	readonly userNameKey: string = "userName";
+	readonly userGenderKey: string = "userGender";
 
-  getLoggedInUser() {
-    const name = localStorage.getItem(this.userNameKey) ?? "";
-    const gender =
-      (localStorage.getItem(this.userGenderKey) as Gender) ?? "robot";
+	getLoggedInUser() {
+		const name = localStorage.getItem(this.userNameKey) ?? "";
+		const gender =
+			(localStorage.getItem(this.userGenderKey) as Gender) ?? "robot";
 
-    if (name) {
-      return { name, gender } as User;
-    }
+		if (name) {
+			return { name, gender } as User;
+		}
 
-    return void 0;
-  }
+		return void 0;
+	}
 
-  logout() {
-    localStorage.removeItem(this.userNameKey);
-    localStorage.removeItem(this.userGenderKey);
-  }
+	logout() {
+		localStorage.removeItem(this.userNameKey);
+		localStorage.removeItem(this.userGenderKey);
+	}
 
-  login(user: User) {
-    localStorage.setItem(this.userNameKey, user.name);
-    localStorage.setItem(this.userGenderKey, user.gender);
-  }
+	login(user: User) {
+		if (!user?.name || !user?.gender) {
+			return;
+		}
+
+		localStorage.setItem(this.userNameKey, user.name);
+		localStorage.setItem(this.userGenderKey, user.gender);
+	}
 }
 
 export const localStorageAuth = new LocalStorageAuthService();
